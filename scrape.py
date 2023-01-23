aefc5a88508ee54105cfad4476a7c6f392537c3b
Rafael Belokurows <rafabelokurows@gmail.com>
2023-01-23 22:02:57 +0000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ import logging
 from linkedin_jobs_scraper import LinkedinScraper
 from linkedin_jobs_scraper.events import Events, EventData
 from linkedin_jobs_scraper.query import Query, QueryOptions, QueryFilters
-from linkedin_jobs_scraper.filters import RelevanceFilters, TimeFilters, TypeFilters, ExperienceLevelFilters, RemoteFilters
+from linkedin_jobs_scraper.filters import RelevanceFilters, TimeFilters, TypeFilters, ExperienceLevelFilters, OnSiteOrRemoteFilters
 import json
 import random
 from webdriver_manager.chrome import ChromeDriverManager
