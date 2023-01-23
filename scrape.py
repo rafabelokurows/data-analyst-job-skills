78dd71a9fe469a16777dc0ca99e6b4cc15c23bd3
Rafael Belokurows <rafabelokurows@gmail.com>
2023-01-23 20:20:06 +0000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -4,6 +4,7 @@ from linkedin_jobs_scraper.events import Events, EventData
 from linkedin_jobs_scraper.query import Query, QueryOptions, QueryFilters
 from linkedin_jobs_scraper.filters import RelevanceFilters, TimeFilters, TypeFilters, ExperienceLevelFilters, RemoteFilters
 import json
+import random
 
 # Change root logger level (default is WARN)
 logging.basicConfig(level = logging.INFO)
@@ -41,7 +42,7 @@ scraper = LinkedinScraper(
     chrome_options=None,  # Custom Chrome options here
     headless=True,  # Overrides headless mode only if chrome_options is None
     max_workers=1,  # How many threads will be spawned to run queries concurrently (one Chrome driver for each thread)
-    slow_mo=0.4,  # Slow down the scraper to avoid 'Too many requests (429)' errors
+    slow_mo=1.2  # Slow down the scraper to avoid 'Too many requests (429)' errors
 )
 
 # Add event listeners
@@ -49,25 +50,40 @@ scraper.on(Events.DATA, on_data)
 scraper.on(Events.ERROR, on_error)
 scraper.on(Events.END, on_end)
 
+
+group_of_items = {'Seattle, Washington, United States',
+'San Francisco, California, United States',
+'United States Remote','European Union Remote',
+'Boston, Massachusetts, United States',
+'Berlin, Germany',
+'London, England, United Kingdom',
+'Canada Remote',
+'Toronto, Ontario, Canada',
+'Vancouver, British Columbia, Canada'}
+num_to_select = 4
+locationsToQuery = random.sample(group_of_items, num_to_select)
+
 queries = [
     Query(
+      query='Data Analyst',
         options=QueryOptions(
-            locations=['United States'],
+            locations=locationsToQuery
             optimize=True,  # Blocks requests for resources like images and stylesheet
-            limit=10,  # Limit the number of jobs to scrape
+            limit=25,  # Limit the number of jobs to scrape
+            skip_promoted_jobs=True,
             filters=QueryFilters(
                 relevance=RelevanceFilters.RECENT,
                 time=TimeFilters.MONTH,
-                type=[TypeFilters.FULL_TIME, TypeFilters.INTERNSHIP],
-                experience=None,                
+                on_site_or_remote=[OnSiteOrRemoteFilters.REMOTE],
+                #type=[TypeFilters.FULL_TIME, TypeFilters.INTERNSHIP],
+                experience=None               
             )
         )
     ),
 ]
 
 scraper.run(queries)
-import os 
-with open(os.getcwd()+'/jobs.json', 'w') as f:
+with open('data/jobs.json', 'w') as f:
     json.dump(cache, f, indent=4)
 
 print(f"Operation completed. Scraped {len(cache)} jobs")
