00f85c14cbbe931ebba5b19c0cd286db2d778c69
Rafael Belokurows <rafabelokurows@gmail.com>
2023-05-03 23:05:56 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,7 @@
+mkdir -p ~/.streamlit/
+echo "[general]"  > ~/.streamlit/credentials.toml
+echo "email = \"rafabelokurows@gmail.com\""  >> ~/.streamlit/credentials.toml
+echo "[server]"  > ~/.streamlit/config.toml 
+echo "headless = true"  >> ~/.streamlit/config.toml
+echo "port = $PORT"  >> ~/.streamlit/config.toml
+echo "enableCORS = false"  >> ~/.streamlit/config.toml
